c998cb05b3bf1b22ab36f28e603cc2c5edc8d51c
Anton Smelchakov <myalysum@gmail.com>
2024-11-24 23:08:32 +0300

    feat: finish gifts page responsivnes

--- christmas-shop/vite.config.js
+++ christmas-shop/vite.config.js
@@ -3,4 +3,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '',
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                gifts: './gifts/gifts.html'
+            },
+        },
+        target: 'esnext'
+    }
 })
