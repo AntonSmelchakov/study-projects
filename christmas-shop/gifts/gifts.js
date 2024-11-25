ec4b605e23dcca7139914c28cae57760839cd2da
Anton Smelchakov <myalysum@gmail.com>
2024-11-25 13:29:47 +0300

    fix: some vite specific pathing quirks

--- christmas-shop/gifts/gifts.js
+++ christmas-shop/gifts/gifts.js
@@ -1,8 +1,9 @@
 import '../node_modules/modern-normalize/modern-normalize.css'
 import '../style.css'
+import './gifts.css'
 import createMainGiftsPanel from '../src/components/bigGiftsPanel';
 
-const gifts = await fetch('../../json/gifts.json').then(resp => resp.json());
+const gifts = await fetch('../json/gifts.json').then(resp => resp.json());
 
 const main = document.querySelector('main');
 
