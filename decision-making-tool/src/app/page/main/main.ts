2fbbae8780088815dc8afa7e3aa9b700561987a6
Anton Smelchakov <myalysum@gmail.com>
2025-03-20 19:58:42 +0300

    feat: latest build with ghpages rerouting

--- decision-making-tool/src/app/page/main/main.ts
+++ decision-making-tool/src/app/page/main/main.ts
@@ -1,7 +1,9 @@
 import type Router from '../../router/router';
 import type StateHandler from '../../state-handler/state-handler';
 import ComplexElement from '../../utils/complex-element';
+import ElementBuilder from '../../utils/element-builder';
 import Index from './index';
+import NotFound from './not-found/not-found';
 import Picker from './picker/picker';
 
 export default class Main extends ComplexElement {
@@ -13,11 +15,16 @@ export default class Main extends ComplexElement {
     super({ tag: 'main', classNames: ['main'] });
     this.index = new Index(stateHandler, router);
     this.picker = new Picker(stateHandler, router);
-    this.notFound = new ComplexElement({ tag: 'div', textContent: 'Wrong page buddy' });
+    this.notFound = new NotFound(router);
     this.configureElement();
   }
 
   public configureElement(): void {
     this.getElement().append(this.index.getElement());
+  }
+
+  public configureNotFound(): void {
+    const button = new ElementBuilder({ tag: 'button', classNames: ['button'] });
+    button.getElement().addEventListener('click', () => {});
   }
 }
